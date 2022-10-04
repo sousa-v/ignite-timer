3831687dce9deedcb63e4460cd38621b9652cf6b
Victor Sousa <victorbrito001@gmail.com>
2022-10-04 16:38:30 -0300

    Ajustes no tempo minimo de um ciclo

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -26,7 +26,7 @@ export function NewCycleForm() {
         type="number"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         {...register('minutesAmount', { valueAsNumber: true })}
         disabled={!!activeCycle}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
     .number()
-    .min(1, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
     .max(60, 'O clico precisa ser de no máximo 60 minutos'),
 })
 
